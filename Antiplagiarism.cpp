f6af9a15397cc725e4389b3d163064fa5018eddb
Evgeny Levdorovich <39227373+levjack1995@users.noreply.github.com>
2021-10-14 23:49:22 +0300

    UpdateMd5

--- Antiplagiarism.cpp
+++ Antiplagiarism.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
+#include "md5.h"
 
 using namespace std;
 
-int main() {
-    
+int main()
+{
+
     cout << "Hello world";
-    
+
     return 0;
 }
+
+// определить количество слов в тексте - метод , он же и будет считать количество выборок
+// string word[words] = {text...}; Создаём массив
+// int main(int argc, const char **argv)
+// {
+//     char text[N];
+//     char word[N];
+//     int i = 0;
+//     int iw = 0;
+//     // int wCounter = 0;
+
+//     cout << "input text" << endl;
+//     cin.getline(text, N);
+//     // AW H
+//     // i = 3 iw = 3
+
+//     for (i = 0; text[i] != '\0'; i++)
+//     {
+//         if (!isSeparator(text[i])){
+// 			word[iw] = text[i];
+// 			iw++;
+// 			if (isSeparator(text[i+1]) or text[i+1] == '\0'){
+// 				word[iw] = '\0';
+// 				// string str[wCounter++] = word;
+// 				cout << word << endl;
+// 				iw = 0;
+// 			}
+//     }
+// }
+//     return 0;
+// }
+
+// for (проверка выборки , равны ли хеши.)
+// >> 2 массива строк.

285a9322aaa3ccc07c3c2d6165c767fca4e8453d
Evgeny Levdorovich <39227373+levjack1995@users.noreply.github.com>
2021-10-15 00:32:44 +0300

    AddBasicFunctionality

--- Antiplagiarism.cpp
+++ Antiplagiarism.cpp
@@ -3,15 +3,28 @@
 
 using namespace std;
 
+int calculateCountOfWords(string str);
+int strLen(char str[]);
+
+bool isSeparator(char ch);
+
+double antiPlagiarism(string text, string fragment);
+
+void putWordsInArray(string text, string strArray[]);
+
+const int N = 128;
+
 int main()
 {
+    string str = "AW 2 3242 fdf, fdf";
+    string strCmp = "AW 2 3242 fdf, fdg";
 
-    cout << "Hello world";
+    double b = antiPlagiarism(str, str);
 
     return 0;
 }
 
-// определить количество слов в тексте - метод , он же и будет считать количество выборок
+// определить количество слов в тексте - метод , он же и будет считать количество выборок ++
 // string word[words] = {text...}; Создаём массив
 // int main(int argc, const char **argv)
 // {
@@ -44,3 +57,80 @@ int main()
 
 // for (проверка выборки , равны ли хеши.)
 // >> 2 массива строк.
+
+// AW 2;
+double antiPlagiarism(string text, string fragment)
+{
+    int arrSizeOfText = calculateCountOfWords(text);
+    int arrSizeOfFragment = calculateCountOfWords(fragment);
+
+    string str[arrSizeOfText];
+    string fragmentStr[arrSizeOfFragment];
+
+    putWordsInArray(text, str);
+    putWordsInArray(fragment, fragmentStr);
+
+    return 0;
+}
+int calculateCountOfWords(string str)
+{
+    int i = 0;
+    int counter = 0;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (!isSeparator(str[i]))
+            if (isSeparator(str[i + 1]) or str[i + 1] == '\0')
+                counter++;
+    }
+    return counter;
+}
+
+bool isSeparator(char ch)
+{
+    char separator[] = {'/', '.', ',', ';', '!', '?', ':', ' '};
+    for (int i = 0; separator[i] != 0; i++)
+    {
+        if (ch == separator[i])
+            return true;
+    }
+    return false;
+}
+
+void putWordsInArray(string text, string strArray[])
+{
+    char word[N];
+    int i = 0;
+    int iw = 0;
+    int wCounter = 0;
+
+    // AW H
+    // i = 3 iw = 3
+
+    for (i = 0; text[i] != '\0'; i++)
+    {
+        if (!isSeparator(text[i]))
+        {
+            word[iw] = text[i];
+            iw++;
+            if (isSeparator(text[i + 1]) or text[i + 1] == '\0')
+            {
+                word[iw] = '\0';
+                strArray[wCounter++] = word;
+                cout << word << endl;
+                iw = 0;
+            }
+        }
+    }
+}
+
+int strLen(char str[])
+{
+    int i = 0;
+    int ch = str[i];
+
+    while (ch != 0)
+    {
+        ch = str[++i];
+    }
+    return i;
+}
